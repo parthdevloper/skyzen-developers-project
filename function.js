256e7e39349b85d306048fe6ffc15d97e9bfad8f
parth7030 <parth.g@pixinvent.com>
2020-12-16 17:08:59 +0530

    swiper Updated

--- function.js
+++ function.js
@@ -41,3 +41,26 @@ function load() {
    </div>
 </div>`;
 }
+
+var mySwiper = new Swiper('.swiper-container', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   slidesPerView: 2,
+   spaceBetween: 10,
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   // And if we need scrollbar
+   scrollbar: {
+      el: '.swiper-scrollbar',
+   },
+})
