3cc43c125de9777e0b2526a5145dcdf09ddd4edb
parth7030 <parth.g@pixinvent.com>
2020-12-16 16:05:20 +0530

    your msg

---
+++ function.js
@@ -0,0 +1,43 @@
+function load() {
+   console.log('loaded');
+   document.getElementById("load-images-here").innerHTML = `<div class="col-12">
+   <div class="row">
+      <div class="col-6">
+         <img class="p-3 gallery-image" src="./assets/gallery/gallery 1.jpg" height="301" width="585"
+            alt="">
+
+      </div>
+      <div class="col-3">
+         <img class="p-3 gallery-image" src="./assets/gallery/gallery 2.jpg" height="301" width="279"
+            alt="">
+
+      </div>
+      <div class="col-3">
+         <img class="p-3 gallery-image" src="./assets/gallery/gallery 3.jpg" height="301" width="279"
+            alt="">
+
+      </div>
+   </div>
+
+</div>
+<div class="col-12">
+   <div class="row">
+
+      <div class="col-3">
+         <img class="p-3 gallery-image" src="./assets/gallery/gallery 4.jpg" height="301" width="279"
+            alt="">
+
+      </div>
+      <div class="col-3">
+         <img class="p-3 gallery-image" src="./assets/gallery/gallery 5.jpg" height="301" width="279"
+            alt="">
+
+      </div>
+      <div class="col-6">
+         <img class="p-3 gallery-image" src="./assets/gallery/gallery 6.jpg" height="301" width="585"
+            alt="">
+
+      </div>
+   </div>
+</div>`;
+}
